62de0942fd2a11654231f381ad504a5fc5fc1011
xdang <xdangleterre@hotmail.com>
2026-02-26 18:38:52 +0100

    Mise en place des routes avec req.params.id ( à lire )

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-console.log("hello node. Comment ça va?")
-
-//on met en place le premier point de terminaison du projet:
-// Ce point de terminaison va renvoyer les résultats des premières requêtes .
-
-// require('express') : on dit à node js d'aller chercher express dans nodes modules 
 const express = require ('express');
-
-//On recrée l'app en utilisant le module express devenu une constante : c'est le petit serveur web sur lequel va fonctionner l'API Rest
 const app = express();
 
-//On définit le port sur lequel on va démarrer notre API Rest
 const port = 3000;
 
-/* 
-===========================================================================
-- On met en place notre premier point de terminaison : C'est le coeur de l'API Rest
-- On utilise une méthode GET et on met en place le chemin de la requête: 
-* C'est le premier argument de la fonction javascript : C'est le chemin quiu va traiter le point de terminaison
-* Le deuxième argument et une fonction qui va traiter de la réponse au client :
-Elle prend deux arguments req et res ( req permet de récupérer l'objet request et res permet d'envoyer la réponse.) 
-On utilise la méthode send de response afin de renvoyer la réponse au client. 
-====================================================================================
- */
 app.get("/", (req,res) => res.send(`Hello , express ! yayyyy !`))
 
-//On lance l'écoute sur le port que l'on a définit (ie => 3000) et on confirme qu'il fonctionne 
+/*
+==========================================================================
+On pourrait définir différents end points , un en point correspoindant à un pokémon 
+sous la forme : app.get(`/api/pokemons/1` , (req,res) => res.send(`Vous avrez demandé le pokémon n°1`)) 
+
+Afin de faciliter les choses on va utiliser la commande req.paramms.id :
+Grâce à cette commande on récupère l'id du pokémon qui nous intéresse, dans la requête client
+On va utiliser cet id dans la réponse .
+=====================================================================================
+
+On va utiliser la syntaxe des : (:id) : express va récupérer l'id du pokémon puis la transmettre dans le point de terminaison
+via l'objet req 
+On peut donc récuorér cette id à la transmettre à la réoponse client.
+*/
+
+
+app.get(`/api/pokemons/:id`, (req,res) => {
+    const id = req.params.id
+    res.send(`Vous avez demandé le pokémon n° :${id}`)
+})
 app.listen(port, () => {
     console.log(`Notre application Node est démarré !
     http://localhost:${port}`);
