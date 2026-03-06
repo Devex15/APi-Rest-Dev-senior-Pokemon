e63e5530087708e1825698f7f9871f5d492b8793
xdang <xdangleterre@hotmail.com>
2026-03-06 13:52:08 +0100

    Mise en place fichier helper avec modif de l'app.js (à lire)

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 const express = require ('express');
+// On charge le tableau du fichier mock-pokemon.js 
+
+const helper = require('./helper.js')
+// On fait appel au module helper.js 
+const pokemons = require('./mock-pokemon.js');
 const app = express();
 
 const port = 3000;
 
 app.get("/", (req,res) => res.send(`Hello , express ! yayyyy !`))
 
-/*
-==========================================================================
-On recherche un pokémon en fonction de son id :
-On va donc chercher grâce à la méthode .params et avec req.params.id , l'id du pokémon
-demandé par l'utilisateur .
+/* On modifie l'app.js afin d'ajouter un message sur l'entête de la réponse :
+res.status(200).json(helper.success(message,pokemon))
 
-Le problème est que l'id est une chaine de caractère , on a la transformer en nombre grâce
-à la propriété parseInt()
-=====================================================================================
-
-La méthode finc() va chercher puis comparer l'id du pokémon demandé avec les id des pokémons du  tableau
+La fonction call back fait appel à deux paramètres car ce sont les deux valeurs sur lesquelles 
+on attend une réponse
 */
-
 
 app.get(`/api/pokemons/:id`, (req,res) => {
     const id = parseInt(req.params.id)
-
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
 
     if (!pokemon) {
         return res.status(404).json({
-        message : "Le pokémon demandé n'a pas été trouvé. " })
+            message: "Pokémon non trouvé"
+        })
     }
-
-    //Avec if () ne pas oublier le return 
-    res.status(200).json(pokemon)
+    
+    const message = "Un pokémon a bien été trouvé."
+    // On ajoute dans la réponse la fonction fonction helper.success() en call back de res.json()
+    res.status(200).json(helper.success(message,pokemon))
 })
 
-app.get(`/`, res.send(`Il y a ${pokemons.length} dans le pokédex.`))
+//nouveau endpoint qui va permettre de retourner le nombre d'éléments du tableau 
+app.get(`/api/pokemons/`, (req,res)=>{
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, en ce moment .`)
+
+})
 
 app.listen(port, () => {
     console.log(`Notre application Node est démarré !
