a5e1e3ccdc75f991ca6d64501c12016191390132
xdang <xdangleterre@hotmail.com>
2026-03-12 18:33:50 +0100

    Modif de /API/Pokemons/ afin d'afficher la liste des pokémons

--- app.js
+++ app.js
@@ -21,22 +21,14 @@ app.get(`/api/pokemons/:id`, (req,res) => {
     }
     
     const message = "Un pokémon a bien été trouvé."
-    // On ajoute dans la réponse la fonction fonction helper.success() en call back de res.json()
-    res.status(200).json.success(message,pokemon)
+    res.status(200).json(success(message,pokemon))
 })
 
-/* Onn simplifie  const helper = require(`./helper.js`) en const {sucess} = require(`./helper.js`) . 
-Grâce à L'ECMA6 (ES6) on utilise les accolades qu'on appelle affectation déstructurée afin de faire appel 
-à la fonction success définie dans le helper.js 
-
-On va simplifier ainsi le res.status(200).json(helper.success(message, pokemon)) en 
-res.status(200).succes(message,pokemon) */
-
-//nouveau endpoint qui va permettre de retourner le nombre d'éléments du tableau 
-app.get(`/api/pokemons/`, (req,res)=>{
-    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, en ce moment .`)
-
+app.get(`/api/pokemons/`, (req,res)=> {
+    const message = "La liste des  pokémons est la suivante :"
+    res.status(200).json(success(message,pokemons))
 })
+
 
 app.listen(port, () => {
     console.log(`Notre application Node est démarré !
